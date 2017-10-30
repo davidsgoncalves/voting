f25b931290664b49e03eaddf3bd66ada0cbf4326
David <davidsg.dev@gmail.com>
2017-10-30 11:27:59 -0200

    add simple form, db configuration, db tables

---
+++ db/migrate/20171030132432_add_competitor.rb
@@ -0,0 +1,20 @@
+class AddCompetitor < ActiveRecord::Migration[5.0]
+  def change
+    create_table :competitors do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+
+    create_table :voters do |t|
+      t.string :name
+      t.string :email
+      t.references :competitors
+      t.boolean :vote
+
+      t.timestamps
+    end
+
+  end
+end
